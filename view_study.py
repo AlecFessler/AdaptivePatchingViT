8c37e9188801c13b34fa74139b01dc991fd34b6c
Alec Fessler <afessler1998@gmail.com>
2024-09-22 12:44:46 -0500

    script for viewing hyperparameter optimization study results

---
+++ view_study.py
@@ -0,0 +1,20 @@
+import optuna
+from optuna.visualization import plot_optimization_history, plot_param_importances, plot_parallel_coordinate, plot_contour
+
+def visualize_study():
+    study = optuna.load_study(study_name="DpsViT_Cifar10", storage="sqlite:///DpsViT_Cifar10.db")
+
+    opt_history = plot_optimization_history(study)
+    opt_history.show()
+
+    param_importances = plot_param_importances(study)
+    param_importances.show()
+
+    parallel_coordinate = plot_parallel_coordinate(study)
+    parallel_coordinate.show()
+
+    contour_plot = plot_contour(study)
+    contour_plot.show()
+
+if __name__ == "__main__":
+    visualize_study()
